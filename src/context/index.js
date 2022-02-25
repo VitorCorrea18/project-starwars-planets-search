bb8aea19cc0f13685640b12296b40c3c29c9706d
vitorcorrea18 <vitorcorrea18@gmail.com>
2022-02-25 16:18:20 -0400

    requisito 2: cria o estado "filteredPlanets" e implementa a funcao "applyInputFilter"

--- src/context/index.js
+++ src/context/index.js
@@ -1,22 +1,40 @@
 import React, { createContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import fetchPlanets from '../services/fetchAPI';
+import applyInputFilter from '../helpers/applyFilters';
 
 export const PlanetContext = createContext();
 
 export const PlanetProvider = ({ children }) => {
   const [data, setData] = useState({}); // useState recebe chaves pois valor inicial do estado é um objeto
+  const [filterInput, setFilterInput] = useState(''); // estado do "filterInput"
+  const [filteredPlanets, setFilteredPlanets] = useState([]); // estado com os planetas filtrados
 
-  const getPlanets = async () => {
+  const getPlanets = async () => { // chama a fetchPlanets para fazer a busca dos dados na API.
     const response = await fetchPlanets();
+    const filterResult = applyInputFilter(response, filterInput); // a função "applyInputFilter" deve receber o retorno da API com todos os planetes mais o valor do input
     setData(response);
+    setFilteredPlanets(filterResult);
   };
 
-  useEffect(() => {
+  useEffect(() => { // "componentdidUpdate", não aceita funções assincronas por isso fiz a "getPlanets"
     getPlanets();
-  }, []); // o lint pede um array, pesquiser o porque depois
+  }, []); // o lint pede um array, pesquisar o porque depois
 
-  const value = { data };
+  const handleInputChange = ({ target }) => {
+    setFilterInput(target.value);
+    const filterResult = applyInputFilter(data, target.value);
+    console.log(target.value);
+    setFilteredPlanets(filterResult);
+  };
+
+  const value = { // passa todos os valores para o componentes filhos, podem ser os estados ou funções
+    data,
+    filterInput,
+    handleInputChange,
+    filteredPlanets,
+  };
+
   return (
     <PlanetContext.Provider value={ value }>
       { children }
