c6248bfc1638f0e2e6d31a04da3a3c372d025445
vitorcorrea18 <vitorcorrea18@gmail.com>
2022-02-24 20:52:46 -0400

    requisito 1: implementa o componente "Table" que e alimentado pelo estado data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { PlanetProvider } from './context/index';
 import './App.css';
+import Table from './components/Table';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <PlanetProvider>
+      <header>
+        <h1>Projeto StarWars Planets</h1>
+      </header>
+      <Table />
+    </PlanetProvider>
   );
 }
 
